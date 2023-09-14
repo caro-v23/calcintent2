02add85d5f1189cef51cc5375fb89864aaa1cb25
emobilis <carolinegatuiri3@gmail.com>
2023-09-14 14:51:39 +0300

    commit

---
+++ app/src/main/java/com/example/calculatorintent/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.calculatorintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calcintent.CalculatorActivity
+import com.example.calculatorintent.ui.theme.CalculatorIntentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Home()
+
+        }
+    }
+}
+
+@Composable
+fun Home() {
+    val context= LocalContext.current
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(androidx.compose.ui.graphics.Color.Red)
+            .padding(16.dp)
+    ) {
+        Text(text = "CalcIntent App",
+            color = androidx.compose.ui.graphics.Color.Black,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 35.sp
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+
+        OutlinedButton(onClick = {context.startActivity(android.content.Intent(context,CalculatorActivity::class.java)) },
+            colors = ButtonDefaults.buttonColors(androidx.compose.ui.graphics.Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Calculator",
+                fontSize = 30.sp)
+
+
+        }
+        Spacer(modifier = Modifier.height(100.dp))
+
+        OutlinedButton(onClick = {
+            context.startActivity(android.content.Intent(context,IntentActivity::class.java))
+        },
+            colors = ButtonDefaults.buttonColors(androidx.compose.ui.graphics.Color.Blue),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "Intents",
+                fontSize = 30.sp)
+
+
+        }
+
+    }
+
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun CalcIntentPreview(){
+    Home()
+}
